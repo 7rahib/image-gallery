030857426ff623a2d941e309d92d6272fd56d38f
7rahib <rahibrashid002@gmail.com>
2023-11-05 14:16:04 +0600

    few bugs fixed

--- src/Component/ImageGallery.js
+++ src/Component/ImageGallery.js
@@ -16,9 +16,12 @@ import './ImageGallery.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 
+//Tailwind CSS classes for image boxes
 const image_box = "w-full rounded border border-2 border-gray-300 overflow-hidden bg-fixed opacity-100 transition duration-300 ease-in-out hover:opacity-50";
+//Tailwind CSS classes for feature image box
 const largerImageClass = "w-full col-span-2 row-span-2 rounded border border-2 border-gray-300 overflow-hidden bg-fixed opacity-100 transition duration-300 ease-in-out hover:opacity-50";
 
+//Giving every image it's ID corresponding to the image source
 const initialImages = [
     { id: 'image1', src: image1 },
     { id: 'image2', src: image2 },
@@ -30,8 +33,7 @@ const initialImages = [
     { id: 'image8', src: image8 },
     { id: 'image9', src: image9 },
     { id: 'image10', src: image10 },
-    { id: 'image11', src: image11 },
-    // ... (add other images)
+    { id: 'image11', src: image11 }
 ];
 
 
@@ -39,6 +41,7 @@ const ImageGallery = () => {
     const [images, setImages] = useState(initialImages);
     const [selectedImages, setSelectedImages] = useState(new Set());
 
+    //Grabbing the image with it's ID and putting it in the React State
     const toggleImageSelection = (imageId) => {
         const updatedSelection = new Set(selectedImages);
 
@@ -51,12 +54,14 @@ const ImageGallery = () => {
         setSelectedImages(updatedSelection);
     };
 
+    // Function for deleting the image (Filtering out the image ID so that it doesn't show)
     const deleteSelectedImages = () => {
         const filteredImages = images.filter((image) => !selectedImages.has(image.id));
         setImages(filteredImages);
         setSelectedImages(new Set());
     };
 
+    //Function for Dragging image by changing image index
     const onDragEnd = (result) => {
         if (!result.destination) {
             return;
@@ -70,10 +75,11 @@ const ImageGallery = () => {
     };
 
     return (
-        <div className="p-5">
+        <div className="max-w-5xl p-5  mx-auto">
             <div className="border">
-                <div className="mx-auto">
+                <div>
                     <div className='p-2 flex justify-between'>
+                        {/* Ternary operator by which number of selected images will be shown if image is */}
                         <div>
                             {selectedImages.size > 0 ?
                                 <>
@@ -83,11 +89,13 @@ const ImageGallery = () => {
                             }
                         </div>
                         <div>
+                            {/* Delete button appears when image is selected */}
                             {selectedImages.size > 0 && (
                                 <button className='text-red-500' onClick={deleteSelectedImages}>Delete Files</button>
                             )}
                         </div>
                     </div>
+                    {/* Made The image boxes draggable and rearrangable by using React DND library */}
                     <DragDropContext onDragEnd={onDragEnd}>
                         <Droppable droppableId="image-gallery" direction="horizontal">
                             {(provided) => (
@@ -96,6 +104,7 @@ const ImageGallery = () => {
                                     ref={provided.innerRef}
                                     className="grid-cols-5 p-10 space-y-2 lg:space-y-0 lg:grid lg:gap-2 lg:grid-rows-1"
                                 >
+                                    {/* Maped all images based on their ID and made them draggable by ID */}
                                     {images.map((image, index) => (
                                         <Draggable key={image.id} draggableId={image.id} index={index}>
                                             {(provided) => (
@@ -105,7 +114,9 @@ const ImageGallery = () => {
                                                     {...provided.dragHandleProps}
                                                     className={index === 0 ? largerImageClass : image_box}
                                                 >
+                                                    {/* Image Grid Box */}
                                                     <div className="image-card">
+                                                        {/* Checkbox appears when hovered over to select*/}
                                                         <input
                                                             type="checkbox"
                                                             checked={selectedImages.has(image.id)}
@@ -113,6 +124,7 @@ const ImageGallery = () => {
                                                             className="image-checkbox"
                                                         />
                                                         <img src={image.src} alt="gallery" className="image-preview" onClick={() => toggleImageSelection(image.id)} />
+                                                        {/* Checkbox appears when selected */}
                                                         {selectedImages.has(image.id) && (
                                                             <div className="checkmark">
                                                                 <FontAwesomeIcon icon={faSquareCheck} />
@@ -123,6 +135,7 @@ const ImageGallery = () => {
                                             )}
                                         </Draggable>
                                     ))}
+                                    {/* Add Image Card which is not draggabble of deletable */}
                                     <div className='w-fullrounded border border-gray-300 overflow-hidden bg-fixed'>
                                         <div className='flex justify-center items-center'>
                                             <div>
